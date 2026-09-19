4fec483c38521a1610e77b06df48b31e6ae0c984
sahaj-cp <sahajpreetsingh316@gmail.com>
2026-09-19 21:12:32 +0530

    Fix import typo and improve login message formatting

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
-import stramlit as st
+import streamlit as st
 st.set_page_config(page_title="login portal",layout='centered')
 st.title("welcome")
 st.subheader("please login the app")
 #username password input
 username=st.text_input("enter username")
-password=st.text_input("enter password")
-type=("password")
+password=st.text_input("enter password",type="password")
 #login button
 if st.button('login'):
   if username == "admin" and password == "12345" :
      st.success("login successfully")
-  st.write(f"Hello{username},your dashboard is ready")
-  
-else:
+     st.write(f"Hello {username},your dashboard is ready")
+  else:
   st.error("you enter wrong password and username please enter again")
