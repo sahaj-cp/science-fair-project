39796f2675cb46ffc908c96de31bffd0e463fb41
sahaj-cp <sahajpreetsingh316@gmail.com>
2026-09-19 21:05:28 +0530

    Fix string formatting in login success message

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ type=("password")
 if st.button('login'):
   if username == "admin" and password == "12345" :
      st.success("login successfully")
-  st.write(f"Hello[username},your dashboard is ready")
+  st.write(f"Hello{username},your dashboard is ready")
   
 else:
   st.error("you enter wrong password and username please enter again")
